8151a45734bfd9e7b2d0099ef5a341f10955df3d
nguyenhuuthanh2k6-wed <nguyenhuuthanh2k6@gmail.com>
2025-12-02 09:34:28 +0700

    quan ly nhap kho

--- inventory.py
+++ inventory.py
@@ -1,20 +1,8 @@
-products = []
-
-def main():
-    while True:
-        print("\n=== INVENTORY MANAGER ===")
-        print("1. Nhập hàng (Thêm sản phẩm)")
-        print("2. Xem tồn kho")
-        print("3. Cảnh báo hết hàng (< 5)")
-        print("0. Thoát")
-
-        choice = input("Chọn chức năng: ")
-
-        if choice == "0":
-            print("Thoát chương trình.")
-            break
-        else:
-            print("Chức năng chưa được triển khai!")
-
-if __name__ == "__main__":
-    main()
+def add_product(name, price, quantity):
+    product = {
+        "name": name,
+        "price": price,
+        "qty": quantity
+    }
+    products.append(product)
+    print("✔ Đã thêm sản phẩm vào kho!")
